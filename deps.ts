edc374fc61f942ffe8c9a7b43ab92d64bbbd1a06
wood chen <wood@czl.net>
2025-02-08 01:56:01 +0800

    Update Deno standard library version to 0.208.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { serve } from "https://deno.land/std@0.220.1/http/server.ts";
-export { crypto } from "https://deno.land/std@0.220.1/crypto/mod.ts";
-export { decode as base64Decode } from "https://deno.land/std@0.220.1/encoding/base64/mod.ts"; 
+export { serve } from "https://deno.land/std@0.208.0/http/server.ts";
+export { crypto } from "https://deno.land/std@0.208.0/crypto/mod.ts";
+export { decode as base64Decode } from "https://deno.land/std@0.208.0/encoding/base64.ts"; 
