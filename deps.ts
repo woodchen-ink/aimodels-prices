546bd2353233e9a759a64755fbd90765da134499
wood chen <wood@czl.net>
2025-02-08 01:54:59 +0800

    Update base64 decoding import path in deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { serve } from "https://deno.land/std@0.220.1/http/server.ts";
 export { crypto } from "https://deno.land/std@0.220.1/crypto/mod.ts";
-export { decode as base64Decode } from "https://deno.land/std@0.220.1/encoding/base64.ts"; 
+export { decode as base64Decode } from "https://deno.land/std@0.220.1/encoding/base64/mod.ts"; 
