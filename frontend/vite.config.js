3c773c7ce038d245ba48bf72182c369d526cbdd1
wood chen <wood@czl.net>
2025-10-10 21:00:14 +0800

    chore(frontend): 添加路径别名解析配置
    在vite配置中添加@路径别名，指向src目录，方便模块引用

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     proxy: {
       '/api': {
